fdfcb050d0435c93531b5952da963f686e1944c1
Peter Ouzts <81652748+pouzts@users.noreply.github.com>
2022-11-12 15:08:23 -0700

    Started work on the AudioManager - 12 November 2022

--- Assets/Scripts/General/AudioManager.cs
+++ Assets/Scripts/General/AudioManager.cs
@@ -1,16 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : Singleton<AudioManager>
 {
-    void Start()
+    [SerializeField] private AudioMixer audioMixer;
+
+    private float masterVolume = 1.0f;
+    private float musicVolume = 1.0f;
+    private float sfxVolume = 1.0f;
+
+    public float MasterVolume
     {
-        
+        get => masterVolume;
+        set 
+        { 
+            masterVolume = Mathf.Clamp01(value);
+        }
     }
 
-    void Update()
+    public float MusicVolume 
     {
-        
+        get => musicVolume;
+        set 
+        { 
+            musicVolume = Mathf.Clamp01(value);
+        } 
+    }
+
+    public float SFXVolume
+    {
+        get => sfxVolume;
+        set
+        {
+            sfxVolume = Mathf.Clamp01(value);
+        }
+    }
+
+    private void Start()
+    {
+        SFXVolume = 0.5f;
+    }
+
+    private void Update()
+    {
+        audioMixer.SetFloat("Master", MasterVolume);
+        audioMixer.SetFloat("Music", MusicVolume);
+        audioMixer.SetFloat("SFX", SFXVolume);
     }
 }
